27bb48e87c5b8e267887991b3f81a2441c5a0b79
Brandon <name.brandon.aguero@gmail.com>
2023-04-30 15:03:23 -0500

    Reparamos el navbar, y terminamos de agregar el main, responsivo(phones, tablets and desktops)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,11 +14,54 @@ async function getData() {
         products: JSON.parse(window.localStorage.getItem("data")) || (await getData()),
         cart: {},
     };
-    console.log(dataObject);
+    //* Contador de los productos disponibles
+    (() => {
+        const amountShirt = document.querySelector(".counter--shirt");
+        const amountHoddie = document.querySelector(".counter--hoddie");
+        const amountSweater = document.querySelector(".counter--sweater");
+        let counterShirt = 0;
+        let counterHoddie = 0;
+        let counterSweater = 0;
+        for (const data of dataObject.products) {
+            if (data.category === "shirt") counterShirt++;
+            if (data.category === "hoddie") counterHoddie++;
+            if (data.category === "sweater") counterSweater++;
+        }
+        amountShirt.textContent = `${counterShirt} products`;
+        amountHoddie.textContent = `${counterHoddie} products`;
+        amountSweater.textContent = `${counterSweater} products`;
+    })();
+    //* Inserción de todos los productos
+    const mainSectionProducts = document.querySelector(".main__section--second");
+    for (const data of dataObject.products) {
+        const article = document.createElement("article");
+        article.classList.add("section__article");
+        const figure = document.createElement("figure");
+        figure.classList.add("section__article--figure");
+        const img = document.createElement("img");
+        img.setAttribute("src", `${data.image}`)
+        const divInfo = document.createElement("div");
+        divInfo.classList.add("section__article--div");
+        const divAdd = document.createElement("div");
+        divAdd.classList.add("article__div--add");
+        divAdd.textContent = "+";
+        const h4 = document.createElement("h4");
+        h4.textContent = `$${data.price}.00`;
+        const span = document.createElement("span");
+        span.textContent = `Stock: ${data.quantity}`;
+        const p = document.createElement("p");
+        p.textContent = `${data.name}`;
+        figure.appendChild(img);
+        h4.appendChild(span);
+        divInfo.append(h4, p, divAdd);
+        article.append(figure, divInfo);
+        mainSectionProducts.append(article);
+    }
+    console.log(mainSectionProducts);
 })();
 
 /* Overcroll Dinámico */
-const headerNav = document.querySelector(".header__nav");
+const headerNav = document.querySelector(".header__div");
 
 function animationScroll() {
     let y = window.scrollY;
@@ -34,10 +77,6 @@ window.onscroll = () => animationScroll();
 
 const body = document.querySelector("body");
 const convertDarkMode = document.querySelectorAll(".convert--darkmode");
-const headerLogo = document.querySelector(".header--logo");
-const moonSun = document.querySelector(".moon--sun");
-const bagColor = document.querySelector(".bag--color");
-const dashBoard = document.querySelector(".dashboard--color");
 
 convertDarkMode.forEach((item) => {
     item.addEventListener("click", darkMode);
@@ -45,4 +84,24 @@ convertDarkMode.forEach((item) => {
 
 function darkMode() {
     body.classList.toggle("darkmode");
+}
+
+/* Desplazamiento del target */
+
+
+const sectionDiv = document.querySelectorAll(".main__section--div");
+
+sectionDiv.forEach((div) => {
+    div.addEventListener("click", moveDivTarget);
+});
+
+function moveDivTarget(e) {
+    sectionDiv.forEach((div) => {
+        div.classList.remove("main__div--target");
+    });
+    if (e.target instanceof HTMLDivElement) {
+        e.target.classList.add("main__div--target");
+    } else {
+        e.target.parentNode.classList.add("main__div--target");
+    }
 }
